4055e356b09e98995a8817f435fa88bba08f46e1
antarac15 <79615669+antarac15@users.noreply.github.com>
2021-06-21 12:41:03 -0700

    Added files

---
+++ LogInForm/js/loginform.js
@@ -0,0 +1,16 @@
+
+
+/*$(document).ready(function() {
+    //$('#username').focus();
+
+    $('#submit').click(function() {
+        //background: #44c4e7 url('images/test.jpg') no-repeat center center fixed;
+
+        event.preventDefault(); // prevent PageReLoad
+        document.write("Player Scores: ")
+
+    });
+});*/
+function myFunction() {
+  document.getElementById("form").style.backgroundImage = "url('gamespot.jpg')";
+  }
